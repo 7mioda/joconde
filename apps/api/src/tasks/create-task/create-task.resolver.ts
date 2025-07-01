973cbc2722e299b223afa7f54d8e78311414f985
Edaly Ahmed <ahmededaly1993@gmail.com>
2025-07-01 14:04:11 +0200

    :sparkles: new subscriptions

--- apps/api/src/tasks/create-task/create-task.resolver.ts
+++ apps/api/src/tasks/create-task/create-task.resolver.ts
@@ -2,13 +2,33 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { TaskService } from '@server/tasks';
 import { CreateTaskInput } from './create-task.input';
 import { Task } from '../task.type';
+import { TrackerService } from '../../shared';
 
 @Resolver(() => Task)
 export class CreateTaskResolver {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(
+    private readonly taskService: TaskService,
+    private readonly trackerService: TrackerService,
+  ) {}
 
   @Mutation(() => Task)
   async createTask(@Args('input') input: CreateTaskInput) {
-    return this.taskService.createTask(input);
+    const task = await this.taskService.createTask(input);
+    this.trackerService.publish({
+      type: 'taskCreated',
+      title: `Task created`,
+      description: `A new task "${task.title}" was created. Check it out and stay updated with your team's progress`,
+      payload: {
+        taskId: task.id,
+        task: {
+          id: task.id,
+          title: task.title,
+          description: task.description,
+          status: task.status,
+          label: task.label,
+        },
+      },
+    });
+    return task;
   }
 } 
--- apps/api/src/tasks/delete-task/delete-tasks.resolver.ts
+++ apps/api/src/tasks/delete-task/delete-tasks.resolver.ts
@@ -1,15 +1,39 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { TaskService } from '@server/tasks';
 import { DeleteTaskOutput } from './delete-task.output';
+import { TrackerService } from '../../shared';
 
 @Resolver(() => DeleteTaskOutput)
 export class DeleteTasksResolver {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(
+    private readonly taskService: TaskService,
+    private readonly trackerService: TrackerService,
+  ) {}
 
   @Mutation(() => DeleteTaskOutput)
   async deleteTask(@Args('id') id: string) {
     try {
+      const task = await this.taskService.getTaskById(id);
+      if (!task) {
+        return { success: false };
+      }
+
       await this.taskService.deleteTask(id);
+      this.trackerService.publish({
+        type: 'taskDeleted',
+        title: `Task deleted`,
+        description: `The task "${task.title}" was deleted. The task has been removed from your workspace`,
+        payload: {
+          taskId: task.id,
+          task: {
+            id: task.id,
+            title: task.title,
+            description: task.description,
+            status: task.status,
+            label: task.label,
+          },
+        },
+      });
       return { success: true };
     } catch (error) {
       return { success: false };
